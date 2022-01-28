b874155d205c9fbc6303bcc580a0082516064fae
Alldayallan <mail@allanreid.co>
2022-01-27 23:24:37 -0800

    first commit

---
+++ login.php
@@ -0,0 +1,45 @@
+<?php 
+    if(isset($_POST["submitButton"])) {
+        echo "Form was submitted";
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Welcom to Reidflix</title>
+    <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+</head>
+
+<body>
+
+    <div class="signInContainer">
+
+        <div class="column">
+            
+            <div class="header">
+                <img src="assets/images/logo.png" title="Logo" alt="Site Logo"/>
+                <h3>Sign In</h3>
+                <span>to continue to Reidflix</span>
+            </div>
+
+            <form method="POST">
+
+                <input type="text" name="username" placeholder="Username" required>
+                
+                <input type="password" name="password" placeholder="Password" required>
+            
+                <input type="submit" name="submitButton" value="SUBMIT">
+
+            </form>
+
+            <a href="register.php" class="signInMessage">Need and accout? Sign up here!</a>
+
+        </div>
+
+    </div>
+    
+</body>
+
+</html>
