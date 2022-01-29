719695dbcaf9ba3523ea1e4a74288102a7f1044a
Alldayallan <mail@allanreid.co>
2022-01-28 23:54:31 -0800

    connecting to database

--- register.php
+++ register.php
@@ -1,7 +1,24 @@
 <?php 
+require_once("includes/config.php");
+require_once("includes/classes/FormSanitizer.php");
+require_once("includes/classes/Account.php");
+
+    $account = new Account($con);
+
     if(isset($_POST["submitButton"])) {
-        echo "Form was submitted";
+
+        $firstName = FormSanitizer::sanitizeFormString($_POST["firstName"]);
+        $lastName = FormSanitizer::sanitizeFormString($_POST["lastName"]);
+        $username = FormSanitizer::sanitizeFormUsername($_POST["username"]);
+        $email = FormSanitizer::sanitizeFormEmail($_POST["email"]);
+        $email2 = FormSanitizer::sanitizeFormEmail($_POST["email2"]);
+        $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
+        $password2 = FormSanitizer::sanitizeFormPassword($_POST["password2"]);
+
+        
     }
+
+    
 ?>
 
 <!DOCTYPE html>
